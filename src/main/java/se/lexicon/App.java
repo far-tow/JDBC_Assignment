e7b69c935ba34263b55557ba12c1db1561feb38f
Farhad Towfighian <fartow@gmail.com>
2022-12-19 16:56:24 +0100

    Initial comment1

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -11,15 +11,11 @@ public class App
 {
 
     public static void main( String[] args ) {
-       /* System.out.println("------------------------------");
-        System.out.println("findById(): ");
-        CityDao dao = new CityDaoJDBC();
+       /*CityDao dao = new CityDaoJDBC();
         City city = dao.findById(143);
         System.out.println("city = " + city);*/
 
         // findByCode()
-        System.out.println("------------------------------");
-        System.out.println("findByCode(): ");
         CityDao dao = new CityDaoJDBC();
         List<City> cities = dao.findByCode("IRN");
         cities.forEach(System.out::println);
